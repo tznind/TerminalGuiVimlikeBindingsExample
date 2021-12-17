833557e4c98901e0e9cf8e152bd41c491a508a60
Thomas Nind <TZNind@dundee.ac.uk>
2021-12-17 10:07:17 +0000

    Initial commit (navigation and Enter to edit mode on TextView)

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿
+
+using System.Collections.Generic;
+using Terminal.Gui;
+
+namespace VimlikeBindings
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Application.Init();
+
+            /*******************************
+             *           |
+             *    lv1    |
+             *           | 
+             *           |
+             *           |
+             * ----------|     textView
+             *           |
+             *           | 
+             *    lv2    |
+             *           |
+             *           |
+             *           |
+             * ****************************/
+
+
+            var window = new Window();
+
+            var lv1 = new ListView(new List<string>{"File 1","File 2"})
+            {
+                Width = Dim.Percent(30),
+                Height= Dim.Percent(50),
+            };
+
+            var lv2 = new ListView(new List<string> { "Operation 1", "Operation 2" })
+            {
+                Y = Pos.Center(),
+                Width = Dim.Percent(30),
+                Height = Dim.Percent(50),
+            };
+
+            var textView = new TextView()
+            {
+                X = Pos.Percent(30) + 1,
+                Width = Dim.Percent(70),
+                Height = Dim.Fill(),
+                Text = "Some text",
+
+                // start the text view in readonly mode so that navigation keybindings 
+                // will not enter text.  In Vim you have to hit Enter to modify the text
+                ReadOnly = true,
+            };
+
+            // navigation keybindings - from lv1 to other controls
+            lv1.KeyDown += (e) =>
+            {
+                if (e.KeyEvent.KeyValue == 'l') // right
+                {
+                    textView.FocusFirst();
+                    e.Handled = true;
+                }
+                if (e.KeyEvent.KeyValue == 'j') // down
+                {
+                    lv2.FocusFirst();
+                    e.Handled = true;
+                }
+            };
+
+            // navigation keybindings - from lv2 to other controls
+            lv2.KeyDown += (e) =>
+            {
+                if (e.KeyEvent.KeyValue == 'l') // right
+                {
+                    textView.FocusFirst();
+                    e.Handled = true;
+                }
+                if (e.KeyEvent.KeyValue == 'k') // up
+                {
+                    lv1.FocusFirst();
+                    e.Handled = true;
+                }
+            };
+
+            // navigation keybindings - from textview to other controls
+            textView.KeyDown += (e) =>
+            {
+                if (e.KeyEvent.KeyValue == 'h') // left
+                {
+                    if(textView.ReadOnly)
+                    {
+                        lv1.FocusFirst();
+                        e.Handled = true;
+                    }
+                }
+                if(e.KeyEvent.Key == Key.Enter) // enter edit mode
+                {
+                    textView.ReadOnly = false;
+                    e.Handled = true;  // TODO: this does not seem to be respected (Enter still appears in control)
+                }
+
+                if (e.KeyEvent.Key == Key.Esc) // leave edit mode
+                {
+                    textView.ReadOnly = true;
+                    e.Handled = true;
+                }
+            };
+            window.Add(lv1);
+            window.Add(lv2);
+            window.Add(textView);
+
+            Application.Top.Add(window);
+
+            Application.Run();
+            Application.Shutdown();
+
+        }
+    }
+}
